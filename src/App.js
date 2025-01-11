79641049719bf6e77cc96e21de2617c2a60b07ba
Leo <leonardoy.siu@gmail.com>
2025-01-10 23:16:01 -0800

    implement switch for light mode / dar mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { Landmark } from 'lucide-react';
+import Switch from './Switch';
+
 
 function App() {
+  const [theme, setTheme] = useState('dark');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    document.documentElement.setAttribute('data-theme', theme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={`min-h-screen ${theme === 'dark' ? 'bg-gradient-to-bl from-background to-purple text-white' : 'bg-gradient-to-bl from-white to-darkBlue text-gray-900'}`}>
+      <header className="py-5 px-10">
+        <Switch isChecked={theme === 'light'} onChange={toggleTheme} />
       </header>
     </div>
   );
 }
 
 export default App;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#1f1d35",
+        darkBlue: '#48497f',
+        purple: '#827ab3',
+        lightBlue: '#b7badc',
+        white: '#b7badc',
+      }
+    },
   },
   plugins: [],
 }
-
